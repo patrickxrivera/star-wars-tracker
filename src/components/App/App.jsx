bb76f552c66da1ba2ae1d055e5a94e4760d262f3
Patrick <patrick.x.rivera@gmail.com>
2018-02-23 15:08:00 -0500

    Styled title

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import styled, { injectGlobal } from 'styled-components';
-import
+import Nav from '../Nav/Nav.jsx';
 
 injectGlobal`
-  @font-face {
+  body {
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen-Sans, Ubuntu, Cantarell, Helvetica Neue,
       sans-serif;
-    color: #fff;
-  }
-
-  body {
     background-color: #4d4566;
+    color: #fff;
+    margin-top: 1rem;
   }
 `;
 
@@ -18,7 +16,6 @@ function App() {
   return (
     <div>
       <Nav />
-      {/* <Home /> */}
     </div>
   );
 }
--- src/components/Nav/Title.jsx
+++ src/components/Nav/Title.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 2px solid red;
+  text-align: center;
+`;
+
+const Header = styled.div`
+  font-size: 1.25rem;
+  font-weight: bold;
+`;
+
+const Subscript = styled.div`
+  font-size: 1.25rem;
+  margin-top: 0.3rem;
+  color: hsl(255, 50%, 85%);
+`;
+
 function Title() {
-  return <div>Title</div>;
+  return (
+    <Container>
+      <Header>Star Wars Tracker</Header>
+      <Subscript>The API Strikes Back</Subscript>
+    </Container>
+  );
 }
 
 export default Title;
