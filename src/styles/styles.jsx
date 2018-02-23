870537ec2b93d1fedc4e3482880efaf1b61983aa
Patrick <patrick.x.rivera@gmail.com>
2018-02-23 14:16:53 -0500

    Created styles module

---
+++ src/styles/styles.jsx
@@ -0,0 +1,15 @@
+const sizes = {
+  desktop: 992,
+  tablet: 768,
+  phone: 376
+};
+
+const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label] / 16}em) {
+      ${css(...args)};
+    }
+  `;
+
+  return acc;
+}, {});
