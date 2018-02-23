edc1ded9df1fb87c4c8e1227173b58df455355c1
Patrick <patrick.x.rivera@gmail.com>
2018-02-23 13:28:20 -0500

    Initialized Nav component

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+class Nav extends Component {
+  render() {
+    return <div>Nav</div>;
+  }
+}
+
+export default Nav;
