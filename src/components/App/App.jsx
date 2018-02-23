344149027031964ebc2b6339fbe43dee8ce649df
Patrick <patrick.x.rivera@gmail.com>
2018-02-23 13:13:52 -0500

    Added font style

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen-Sans, Ubuntu, Cantarell, Helvetica Neue,
+    sans-serif;
+`;
+
+function App() {
+  return <Container>Hello world!</Container>;
+}
+
+export default App;
