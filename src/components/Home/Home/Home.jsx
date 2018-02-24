d8c78db269c7efdaa900ba54c1fe3b2e23d30fdf
Patrick <patrick.x.rivera@gmail.com>
2018-02-24 16:45:33 -0500

    Added this binding for functions and put component in a class

--- src/components/Home/Home/Home.jsx
+++ src/components/Home/Home/Home.jsx
@@ -1,46 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import icons from './HomeData.jsx';
-import { Container, Title, Subscript, CategoriesContainer, CategoryStyles, CategoryName } from './Home.styles.jsx';
-
-const getJSXForCategories = () => {
-  const categoriesJSX = Object.keys(icons).map((categoryName) => {
-    const iconData = getIconDataFrom(icons, categoryName);
-    const categoryJSX = createJSXFrom(iconData);
-    return categoryJSX;
-  });
-
-  return categoriesJSX;
-};
-
-const getIconDataFrom = (icons, categoryName) => {
-  const [color, Icon] = [icons[categoryName].color, icons[categoryName].iconName];
-  return { color, Icon, categoryName };
-};
-
-const createJSXFrom = ({ color, Icon, categoryName }) => {
-  return (
-    <Category key={categoryName} color={color}>
-      <Icon size={35} />
-      <CategoryName>{categoryName}</CategoryName>
-    </Category>
-  );
-};
+import {
+  Container,
+  Title,
+  Subscript,
+  CategoriesContainer,
+  CategoryStyles,
+  CategoryName
+} from './Home.styles.jsx';
 
 const Category = styled.div`
   ${CategoryStyles};
 `;
 
-const Home = () => {
-  const categoriesJSX = getJSXForCategories();
+class Home extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <Container>
-      <Title>Select a category</Title>
-      <Subscript>Detailed information on your favorite Star Wars characters.</Subscript>
-      <CategoriesContainer>{categoriesJSX}</CategoriesContainer>
-    </Container>
-  );
-};
+    [
+      'handleClick',
+      'getJSXForCategories',
+      'getIconDataFrom',
+      'createJSXFrom'
+    ].forEach((propToBind) => {
+      this[propToBind] = this[propToBind].bind(this);
+    });
+  }
+
+  handleClick() {
+    return null;
+  }
+
+  getJSXForCategories() {
+    const categoriesJSX = Object.keys(icons).map((categoryName) => {
+      const iconData = this.getIconDataFrom(icons, categoryName);
+      const categoryJSX = this.createJSXFrom(iconData);
+      return categoryJSX;
+    });
+
+    return categoriesJSX;
+  }
+
+  getIconDataFrom(icons, categoryName) {
+    const [color, Icon] = [
+      icons[categoryName].color,
+      icons[categoryName].iconName
+    ];
+    return { color, Icon, categoryName };
+  }
+
+  createJSXFrom({ color, Icon, categoryName }) {
+    return (
+      <Category
+        onClick={this.handleClick}
+        key={categoryName}
+        color={color}
+        data-test={`category-${categoryName}`}
+      >
+        <Icon size={35} />
+        <CategoryName>{categoryName}</CategoryName>
+      </Category>
+    );
+  }
+
+  render() {
+    const categoriesJSX = this.getJSXForCategories();
+
+    return (
+      <Container>
+        <Title>Select a category</Title>
+        <Subscript>
+          Detailed information on your favorite Star Wars characters.
+        </Subscript>
+        <CategoriesContainer>{categoriesJSX}</CategoriesContainer>
+      </Container>
+    );
+  }
+}
 
 export default Home;
