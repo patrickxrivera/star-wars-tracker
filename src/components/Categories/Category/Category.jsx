afea435b39ae45121b87c0d147c9ac3be807231f
Patrick <patrick.x.rivera@gmail.com>
2018-02-25 00:18:47 -0500

    Added prop-types

--- src/components/Categories/Category/Category.jsx
+++ src/components/Categories/Category/Category.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { ContainerStyles, CategoryName } from './CategoryStyles.jsx';
+import { func, string } from 'prop-types';
 
 const Container = styled.div`
   ${ContainerStyles};
@@ -16,5 +17,12 @@ const Category = ({ onClick, categoryName, color, Icon }) => (
     <CategoryName>{categoryName}</CategoryName>
   </Container>
 );
+
+Category.propTypes = {
+  onClick: func.isRequired,
+  categoryName: string.isRequired,
+  color: string.isRequired,
+  Icon: func.isRequired
+};
 
 export default Category;
