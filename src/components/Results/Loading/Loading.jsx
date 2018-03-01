c1545242aad21bcbfe0e941708e0cc6bb18ac166
Patrick <patrick.x.rivera@gmail.com>
2018-02-28 22:10:10 -0500

    Finalized loading

--- src/components/Results/Loading/Loading.jsx
+++ src/components/Results/Loading/Loading.jsx
@@ -3,16 +3,30 @@ import React from 'react';
 import ReactLoading from 'react-loading';
 import Wrapper from './LoadingStyles.jsx';
 
-const Loading = ({ selected }) => (
-  <Wrapper selected={selected}>
-    <ReactLoading
-      delay={0}
-      type={'bubbles'}
-      color={'#00cec9'}
-      height={100}
-      width={100}
-    />
-  </Wrapper>
-);
+const colorMapping = {
+  People: '#4f5ef5',
+  Planets: '#f44c67',
+  Vehicles: '#2fca74'
+};
+
+const getColorFrom = selected => {
+  return colorMapping[selected];
+};
+
+const Loading = ({ selected }) => {
+  const color = getColorFrom(selected) || '#0984e3';
+
+  return (
+    <Wrapper selected={selected}>
+      <ReactLoading
+        delay={0}
+        type={'bubbles'}
+        color={color}
+        height={100}
+        width={100}
+      />
+    </Wrapper>
+  );
+};
 
 export default Loading;
